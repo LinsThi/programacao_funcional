6c7fff34344ea39a893a3d7c47ae0f92519517fe
Thiago Lins <75625915+LinsThi@users.noreply.github.com>
2021-07-06 18:24:54 -0300

    Update iterate.hs

--- InfiniteLists/iterate.hs
+++ InfiniteLists/iterate.hs
@@ -8,12 +8,7 @@ gerador2 = iterate iterator 1
 gerador3 = [2^n | n <- [0..]]
 --gerador3 = iterate (*2) 1
 
-listaVerf = unfoldr (\b -> if b == 0 then Nothing else Just (b, b `div` 2))
-gerador4 n = reverse (filter(/= 0) [verif n x | x <- lista n] )
-        where
-          lista j = takeWhile (< j+1) (iterate (+1) 0)
-          verif j k = if k `elem` listaVerf j then k else 0
+gerador4 n = takeWhile (>0) $ iterate (`div` 2) n
 gerador5 = unfoldr (\b -> if b == 0 then Nothing else Just (b, b `div` 2))
-
 
 digitos = reverse . unfoldr (\n -> if n == 0 then Nothing else Just (n `mod` 10, n `div` 10))
